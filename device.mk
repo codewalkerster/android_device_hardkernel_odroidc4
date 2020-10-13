3725dc61c890bd455e6e2878ccffe7d12d533ae7
Chris <codewalker@hardkernel.com>
2020-10-13 12:12:46 +0900

    ODROID-C4: Add sys.force_reboot
    Change-Id: Iee66eabc870fe4e29bab420027b08782c9edfbf2

--- device.mk
+++ device.mk
@@ -135,4 +135,5 @@ PRODUCT_COPY_FILES += \
     device/hardkernel/odroidc4/files/default.prop.template:vendor/etc/default.prop.template \
     device/hardkernel/odroidc4/files/makedefaultprop:vendor/bin/makedefaultprop \
     device/hardkernel/odroidc4/files/env.ini.template:vendor/etc/env.ini.template \
+    device/hardkernel/odroidc4/files/makepanic:vendor/bin/makepanic \
     device/hardkernel/odroidc4/recovery/update_ini.sh:vendor/bin/update_ini.sh
